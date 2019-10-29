85c54730f2b9f2e659bb5172c2ece6f3f55547ba
Jayson Gamitin <ahzelgamitin@gmail.com>
2019-10-29 20:08:56 +0800

    asdasd

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Storage;
 
 class MainController extends Controller
 {
