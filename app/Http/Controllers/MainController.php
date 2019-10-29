e96bf732546d75b648432486c66031446e613c29
Jayson Gamitin <ahzelgamitin@gmail.com>
2019-10-29 14:21:12 +0800

    adasd

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,10 +8,12 @@ class MainController extends Controller
 {
     public function receive(Request $request)
 	{
-	        $data = $request->all();
-	        //get the user’s id
-	        $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-	     	$this->sendTextMessage($id, "Hello");
+	      //   $data = $request->all();
+	      //   //get the user’s id
+	      //   $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+	     	// $this->sendTextMessage($id, "Hello");
+		echo "string";
+
 	}
 	private function sendTextMessage($recipientId, $messageText)
     {
--- app/Http/Middleware/VerifyMiddleware.php
+++ app/Http/Middleware/VerifyMiddleware.php
@@ -17,7 +17,7 @@ class VerifyMiddleware
     public function handle($request, Closure $next)
     {
         if ($request->input("hub_mode") === "subscribe"
-            && $request->input("hub_verify_token") === env("MESSENGER_VERIFY_TOKEN")) {
+            && $request->input("hub_verify_token") === "mytoken")) {
             return response($request->input("hub_challenge"), 200);
         }
         return $next($request);
