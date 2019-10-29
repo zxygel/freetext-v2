fb7e949a009b50fdf8240272fd43180170ab5a37
Jayson Gamitin <ahzelgamitin@gmail.com>
2019-10-29 13:40:44 +0800

    update 2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,6 +11,25 @@ class MainController extends Controller
 	        $data = $request->all();
 	        //get the user’s id
 	        $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-	     $this->sendTextMessage($id, "Hello");
+	     	$this->sendTextMessage($id, "Hello");
+	}
+	private function sendTextMessage($recipientId, $messageText)
+    {
+        $messageData = [
+            "recipient" => [
+                "id" => $recipientId
+            ],
+            "message" => [
+                "text" => $messageText
+            ]
+        ];
+        $ch = curl_init('https://graph.facebook.com/v4.0/me/messages?access_token=EAAOUraXZAo4IBALyYDOjWLfsMWhTOcbPOnUtphaR9EgtW8eo9ZCkTDS3GOFpYp4ZCuFxPt7ckdlbroxXeOSl7ZAfZC4IkgBN4VV2ZBZAYi51uwNaurQzv0bjoZAEnOFSpZCZBlhNyZB7L4HlXZCsJYvAd6t1oNumyOo17ZAp9oZB3o4dd5XQZDZD');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageData));
+        curl_exec($ch);
+    
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,22 @@
 |
 */
 
-// Route::get('/', 'HomeController@welcome')->name('welcome');
+Route::get('/', 'HomeController@welcome')->name('welcome');
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/profile', 'ProfileController@index')->name('profile');
-// Route::post('/profile/update', 'ProfileController@update')->name('update');
-// Route::get('/profile/{id}', 'ProfileController@form')->name('form');
+Route::get('/profile', 'ProfileController@index')->name('profile');
+Route::post('/profile/update', 'ProfileController@update')->name('update');
+Route::get('/profile/{id}', 'ProfileController@form')->name('form');
 
-// Route::get('/lessons', 'LessonController@index')->name('lessons');
-// Route::get('/lessons/form', 'LessonController@form')->name('lessons-form');
-// Route::get('/lessons/view/{id}', 'LessonController@view');
-// Route::post('/lessons/add', 'LessonController@create')->name('lessons-add');
-// Route::post('/lessons/update', 'LessonController@update')->name('lessons-update');
-// Route::post('/lessons/delete', 'LessonController@delete')->name('lessons-delete');
+Route::get('/lessons', 'LessonController@index')->name('lessons');
+Route::get('/lessons/form', 'LessonController@form')->name('lessons-form');
+Route::get('/lessons/view/{id}', 'LessonController@view');
+Route::post('/lessons/add', 'LessonController@create')->name('lessons-add');
+Route::post('/lessons/update', 'LessonController@update')->name('lessons-update');
+Route::post('/lessons/delete', 'LessonController@delete')->name('lessons-delete');
 
-// Route::get('/assignments', 'AssignmentController@index')->name('assignments');
+Route::get('/assignments', 'AssignmentController@index')->name('assignments');
 //route for verification
 Route::get("/text", "MainController@receive")->middleware("verify");
 
