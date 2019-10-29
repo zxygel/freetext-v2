fe2536d160ca31ec3c547f19ea06ab683bbb03a3
Jayson Gamitin <ahzelgamitin@gmail.com>
2019-10-29 14:22:48 +0800

    adasd

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,12 +8,11 @@ class MainController extends Controller
 {
     public function receive(Request $request)
 	{
-	      //   $data = $request->all();
-	      //   //get the user’s id
-	      //   $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-	     	// $this->sendTextMessage($id, "Hello");
-		echo "string";
-
+	        $data = $request->all();
+	        //get the user’s id
+	        $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+	     	$this->sendTextMessage($id, "Hello");
+	     	
 	}
 	private function sendTextMessage($recipientId, $messageText)
     {
